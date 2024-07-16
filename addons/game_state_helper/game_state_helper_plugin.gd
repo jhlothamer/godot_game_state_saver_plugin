726d27755ee04c643745c12d0e2d0862308034d9
Jason Lothamer <jhlothamer@gmail.com>
2024-07-16 07:00:36 -0400

    fix static type issues in plugin scripts

--- addons/game_state_helper/game_state_helper_plugin.gd
+++ addons/game_state_helper/game_state_helper_plugin.gd
@@ -6,9 +6,9 @@ const GAME_STATE_SERVICE_PATH = "res://addons/game_state_helper/game_state_servi
 const GAME_STATE_SERVICE_AUTLOAD_NAME = "GameStateService"
 
 
-func _enable_plugin():
+func _enable_plugin() -> void:
 	add_autoload_singleton(GAME_STATE_SERVICE_AUTLOAD_NAME, GAME_STATE_SERVICE_PATH)
 
 
-func _disable_plugin():
+func _disable_plugin() -> void:
 	remove_autoload_singleton(GAME_STATE_SERVICE_AUTLOAD_NAME)
--- addons/transition_mgr/transition_mgr_plugin.gd
+++ addons/transition_mgr/transition_mgr_plugin.gd
@@ -6,9 +6,9 @@ const TRANSITION_MGR_PATH = "res://addons/transition_mgr/transition_mgr.tscn"
 const TRANSITION_MGR_AUTLOAD_NAME = "TransitionMgr"
 
 
-func _enable_plugin():
+func _enable_plugin() -> void:
 	add_autoload_singleton(TRANSITION_MGR_AUTLOAD_NAME, TRANSITION_MGR_PATH)
 
 
-func _disable_plugin():
+func _disable_plugin() -> void:
 	remove_autoload_singleton(TRANSITION_MGR_AUTLOAD_NAME)
