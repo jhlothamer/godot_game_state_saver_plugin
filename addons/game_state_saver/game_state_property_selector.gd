5e80c00a1bb5fa9dc6cad60a61134817765e46d7
Ziboo <r.ziboo@gmail.com>
2026-02-24 10:15:23 -0500

    Update scripts based on PR Review

--- addons/game_state_saver/game_state_property_selector.gd
+++ addons/game_state_saver/game_state_property_selector.gd
@@ -1,74 +1,73 @@
 class_name GameStatePropertySelector
 extends EditorProperty
 
-var option_button := OptionButton.new()
-var parent_node: Node
-var game_state_helper : GameStateHelper
-var search_bar := LineEdit.new()
+var _parent_node: Node
+var _game_state_helper : GameStateHelper
+var _search_bar := LineEdit.new()
 
-var button := Button.new()
-var popup := PopupPanel.new()
-var list := ItemList.new()
-var all_props: Array[String] = []
+var _button := Button.new()
+var _popup := PopupPanel.new()
+var _list := ItemList.new()
+var _all_props: Array[String] = []
 
-func setup(_game_state_helper: Node)  -> void:
-	game_state_helper = _game_state_helper
-	parent_node = _game_state_helper.get_parent()
+func setup(game_state_helper: Node)  -> void:
+	_game_state_helper = game_state_helper
+	_parent_node = game_state_helper.get_parent()
 	_refresh_property_list()
 
 func _init() -> void:
-	self.name_split_ratio = 0
-	add_child(button)
-	add_focusable(button)
-	button.text = "+++ Add Property +++"
-	button.add_theme_color_override("font_color", Color(0, 0.8, 0))
+	if "name_split_ratio" in self: self.name_split_ratio = 0
+	add_child(_button)
+	add_focusable(_button)
+	_button.text = "+++ Add Property +++"
+	_button.add_theme_color_override("font_color", Color(0, 0.8, 0))
 	
-	button.pressed.connect(_on_button_pressed)
+	_button.pressed.connect(_on_button_pressed)
 	
 	# Setup Popup UI
 	var vbox : VBoxContainer = VBoxContainer.new()
-	search_bar.placeholder_text = "Filter properties..."
-	search_bar.text_changed.connect(_on_search_changed)
+	_search_bar.placeholder_text = "Filter properties..."
+	_search_bar.text_changed.connect(_on_search_changed)
 	
-	list.custom_minimum_size = Vector2(200, 600)
-	list.item_selected.connect(_on_item_selected)
+	_list.custom_minimum_size = Vector2(200, 600)
+	_list.item_selected.connect(_on_item_selected)
 	
-	vbox.add_child(search_bar)
-	vbox.add_child(list)
-	popup.add_child(vbox)
-	add_child(popup)
+	vbox.add_child(_search_bar)
+	vbox.add_child(_list)
+	_popup.add_child(vbox)
+	add_child(_popup)
 	
 func _refresh_property_list() -> void:
-	all_props.clear()
-	if not parent_node: return
+	_all_props.clear()
+	if not _parent_node: return
 	
-	for prop in parent_node.get_property_list():
+	for prop in _parent_node.get_property_list():
 		if prop.usage & PROPERTY_USAGE_EDITOR:
-			if prop.name not in game_state_helper.save_properties:
-				all_props.append(prop.name)
+			if prop.name not in _game_state_helper.save_properties:
+				_all_props.append(prop.name)
 	_update_list("")
 		
 func _on_search_changed(new_text: String) -> void:
 	_update_list(new_text)
 
 func _update_list(filter: String) -> void:
-	list.clear()
-	for p_name in all_props:
+	_list.clear()
+	for p_name in _all_props:
 		if filter == "" or filter.to_lower() in p_name.to_lower():
-			list.add_item(p_name)
+			_list.add_item(p_name)
 				
 func _on_button_pressed() -> void:
 	_refresh_property_list()
-	popup.popup_on_parent(Rect2(button.global_position + Vector2(0, button.size.y), Vector2(button.size.x, 300)))
-	_update_list(search_bar.text)
-	search_bar.grab_focus()
-	search_bar.select_all()
+	_popup.popup_on_parent(Rect2(_button.global_position + Vector2(0, _button.size.y), Vector2(_button.size.x, 300)))
+	_update_list(_search_bar.text)
+	_search_bar.grab_focus()
+	_search_bar.select_all()
 	
 
 func _on_item_selected(index: int) -> void:
-	var selected_name : String = list.get_item_text(index)
-	game_state_helper.save_properties.append(selected_name)
+	var selected_name : String = _list.get_item_text(index)
+	_game_state_helper.save_properties.append(selected_name)
 	emit_changed("save_properties",selected_name)
 	#search_bar.text = ""
-	popup.hide()
+	_popup.hide()
 	
--- addons/game_state_saver/game_state_saver_plugin.gd
+++ addons/game_state_saver/game_state_saver_plugin.gd
@@ -6,14 +6,14 @@ const GAME_STATE_SERVICE_PATH = "res://addons/game_state_saver/game_state_servic
 const GAME_STATE_SERVICE_AUTLOAD_NAME = "GameStateService"
 
 const GAME_STATE_INSPECTOR_PLUGIN = "res://addons/game_state_saver/game_sate_inspector.gd"
-var gamestateinspector_plugin : GameStateInspector 
+var _gamestateinspector_plugin : GameStateInspector 
 
 func _enter_tree() -> void:
-	gamestateinspector_plugin = load(GAME_STATE_INSPECTOR_PLUGIN).new()
-	add_inspector_plugin(gamestateinspector_plugin)
+	_gamestateinspector_plugin = load(GAME_STATE_INSPECTOR_PLUGIN).new()
+	add_inspector_plugin(_gamestateinspector_plugin)
 
 func _exit_tree() -> void:
-	remove_inspector_plugin(gamestateinspector_plugin)
+	remove_inspector_plugin(_gamestateinspector_plugin)
 
 
 func _enable_plugin() -> void:
